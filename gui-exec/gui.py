d6c687013e3b9d70a5aef81c59ae85f58de2e93e
Asif Rehan <asif.rehan9@gmail.com>
2014-10-19 02:38:00 -0400

    gui-exec added

---
+++ gui-exec/gui.py
@@ -0,0 +1,13 @@
+'''
+Created on Oct 19, 2014
+
+@author: asr13006
+'''
+from Tkinter import *
+
+frmMain = Tk()
+
+label = Label(frmMain, text="Welcome to py2exe!")
+label.pack()
+
+frmMain.mainloop()
---
+++ gui-exec/setup.py
@@ -0,0 +1,11 @@
+'''
+Created on Oct 19, 2014
+
+@author: asr13006
+'''
+
+from distutils.core import setup
+
+import py2exe
+
+setup(console=["gui.py"])
