1da0a5ba8aa20d17dbd53698a066693a97d9a80c
Asif Rehan <asif.rehan9@gmail.com>
2014-10-19 02:09:35 -0400

    exeutable added

---
+++ GUIpack/GUIpack.py
@@ -0,0 +1,18 @@
+'''
+Created on Oct 19, 2014
+
+@author: asr13006
+'''
+print "Python script to exe test program"
+
+count = 0
+
+while count < 10:
+    print "count = " + str(count) +"\n"
+    count = count + 1
+
+
+
+
+if __name__ == '__main__':
+    pass
